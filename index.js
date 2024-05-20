3453b3a469e646981ca83f9dc350fa9d436d8c92
Emiliano Portillo <EmilianoMt>
2024-05-20 07:25:38 -0600

    Cambios

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
+const db = require("./config/database");
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
 app.get("/", (req, res, next) => {
-  console.log(req);
-  res.send("Hello World!");
+  // console.log(req);
+  res.render("index");
 });
 
-app.get("/pokemon", (req, res, next) => {
-  res.json({ name: "Pikachu", type: "Electric" });
+app.get("/pokemon", async (req, res, next) => {
+  const pokemon = await db.query("SELECT * FROM pokemon");
+  // console.log(pokemon)
+  res.json(pokemon);
 });
+
+app.post("/pokemon", (req, res, next) => {
+  console.log(req.body);
+  // res.send("Informacion guardada (o no)");
+  res.json(req.body);	
+});
+
 
 app.listen(3000, () => {
   console.log("Server is running...");
